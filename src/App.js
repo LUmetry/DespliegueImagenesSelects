20415ac440d69e649777dcddf636ccfd2294fa58
LUmetry <miguelelpapichulo2014@gmail.com>
2026-07-19 19:54:08 -0400

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useState } from "react";
+import Select from "./select";
 
-function App() {
+export default function App() {
+  const [pagina, setPagina] = useState("inicio");
+
+
+  const fotosPaisajes = ['Playa2.jpg', 'Banco.jpg','Lago.jpg','Nubes.jpg','Playa.jpg']
+  const [fotoSeleccionada, setFotoSeleccionada] = React.useState(0)
+
+  const fotosUnibe = ['Unibe1.jpg', 'Unibe2.jpg', 'Unibe3.jpg', 'Unibe4.jpg']
+  const [fotoUnibeSeleccionada, setFotoUnibeSeleccionada] = React.useState(0)
+  
+
+  function fotoSiguiente() {
+    if (fotoSeleccionada < fotosPaisajes.length - 1) {
+      setFotoSeleccionada(fotoSeleccionada + 1)
+    }
+  }
+  function fotoPrevia() {
+    if (fotoSeleccionada > 0) {
+      setFotoSeleccionada(fotoSeleccionada - 1)
+    }
+  }
+
+  function fotoUnibeSiguiente() {
+    if (fotoUnibeSeleccionada < fotosUnibe.length - 1) {
+      setFotoUnibeSeleccionada(fotoUnibeSeleccionada + 1)
+    }
+  }
+  function fotoUnibePrevia() {
+    if (fotoUnibeSeleccionada > 0) {
+      setFotoUnibeSeleccionada(fotoUnibeSeleccionada - 1)
+    }
+
+
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => setPagina("inicio")}>Inicio</button>
+      <button onClick={() => setPagina("autos")}>Autos</button>
+
+      {pagina === "inicio" && (
+        <div>
+          <h2>Fotos de paisajes tomadas por mi</h2>
+
+          <input type="button" value="<" onClick={fotoPrevia} />
+          <input type="button" value=">" onClick={fotoSiguiente} />
+
+          <p>
+            <img
+              src={process.env.PUBLIC_URL + "/img/" + fotosPaisajes[fotoSeleccionada]}
+              alt="paisaje"
+              width={500}
+            />
+          </p>
+
+          <p>{fotosPaisajes[fotoSeleccionada]}</p>
+
+          <hr />
+
+          <h2>Fotos Unibe</h2>
+
+          <input type="button" value="<" onClick={fotoUnibePrevia} />
+          <input type="button" value=">" onClick={fotoUnibeSiguiente} />
+
+          <p>
+            <img
+              src={require(`./imgs/${fotosUnibe[fotoUnibeSeleccionada]}`)}
+              alt="foto unibe"
+              width={500}
+            />
+          </p>
+
+          <p>{fotosUnibe[fotoUnibeSeleccionada]}</p>
+        </div>
+      )}
+
+      {pagina === "autos" && <Select/>}
     </div>
   );
 }
-
-export default App;
---
+++ src/datos.js
@@ -0,0 +1,93 @@
+const rubros = [
+  {
+    codigo: 1,
+    nombre: 'Sedan'
+  },
+  {
+    codigo: 2,
+    nombre: 'SUV'
+  },
+  {
+    codigo: 3,
+    nombre: 'Coupé'
+  }
+]
+
+const articulos = [
+  {
+    codigo: 1,
+    codigorubro: 1,
+    nombre: 'Mercedes-Benz C-Class',
+    precio: 45000
+  },
+  {
+    codigo: 2,
+    codigorubro: 1,
+    nombre: 'BMW Serie 3',
+    precio: 47000
+  },
+  {
+    codigo: 3,
+    codigorubro: 1,
+    nombre: 'Audi A4',
+    precio: 43000
+  },
+  {
+    codigo: 4,
+    codigorubro: 1,
+    nombre: 'Porsche Panamera',
+    precio: 92000
+  },
+  {
+    codigo: 5,
+    codigorubro: 2,
+    nombre: 'Mercedes-Benz GLC',
+    precio: 54000
+  },
+  {
+    codigo: 6,
+    codigorubro: 2,
+    nombre: 'BMW X3',
+    precio: 52000
+  },
+  {
+    codigo: 7,
+    codigorubro: 2,
+    nombre: 'Audi Q5',
+    precio: 51000
+  },
+  {
+    codigo: 8,
+    codigorubro: 2,
+    nombre: 'Porsche Macan',
+    precio: 66000
+  },
+  {
+    codigo: 9,
+    codigorubro: 3,
+    nombre: 'Mercedes-Benz E Coupé',
+    precio: 73000
+  },
+  {
+    codigo: 10,
+    codigorubro: 3,
+    nombre: 'BMW Serie 4 Coupé',
+    precio: 69000
+  },
+  {
+    codigo: 11,
+    codigorubro: 3,
+    nombre: 'Audi A5 Coupé',
+    precio: 67000
+  },
+  {
+    codigo: 12,
+    codigorubro: 3,
+    nombre: 'Porsche 911 Carrera',
+    precio: 105000
+  }
+]
+
+
+export {articulos}
+export {rubros}
